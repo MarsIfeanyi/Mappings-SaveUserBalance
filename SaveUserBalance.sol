a986b5ff349cc2ee6dbab8617e9fc9878c5120b0
MarsIfeanyi <marschalice@gmail.com>
2022-12-22 15:27:44 +0100

    Add comments

--- SaveUserBalance.sol
+++ SaveUserBalance.sol
@@ -2,20 +2,17 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-/**
- * A smart contract that saves user balance.
- * This contract has functions that saves the amount a user is depositing into a mapping and a function that searches for user balance inside the mapping and returns the balance of who calls the contract.
- */
-
 contract SaveUserBalance {
     //mapping of address to uint256
     mapping(address => uint256) public balances;
     address public owner = msg.sender;
 
+    // functions that saves the amount a user is depositing into a mapping
     function deposit(uint256 amount) public {
         balances[msg.sender] += amount;
     }
 
+    //function that searches for user balance inside the mapping and returns the balance of who calls the contract
     function checkBalance() public view returns (uint) {
         return balances[msg.sender];
     }
